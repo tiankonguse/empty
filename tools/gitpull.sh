bd938638e84b5b6207c0458a7e90b68a86d8800b
tiankonguse <i@tiankonguse.com>
2015-03-31 00:07:14 +0800

    update

---
+++ tools/gitpull.sh
@@ -0,0 +1,12 @@
+#########################################################################
+# File Name: gitpull.sh
+# Author: tiankonguse
+# mail: i@tiankonguse.com
+# Created Time: 2015年03月30日 星期一 21时43分35秒
+#########################################################################
+#!/bin/bash
+
+cmd="git pull origin master";
+echo "==>$cmd";
+eval $cmd;
+
